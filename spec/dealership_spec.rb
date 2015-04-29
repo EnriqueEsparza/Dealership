4283d930cf666795e003f1693347dda9534caf4a
enriqueesparza & Paul <student@epicodus.com>
2015-04-29 11:33:42 -0700

    Pass first five initial specs

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -12,4 +12,38 @@ describe(Dealership) do
       expect(test_dealership.name()).to(eq("Bob's Used Cars"))
     end
   end
+
+  describe('#id') do
+    it("returns the id of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.id()).to(eq(1))
+    end
+  end
+
+  describe('#cars') do
+    it("initially returns an empty array of cars for the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.cars()).to(eq([]))
+    end
+  end
+
+  describe('.all') do
+    it("is empty at first") do
+      expect(Dealership.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("adds a dealership to the array of saved dealerships") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      test_dealership.save()
+      expect(Dealership.all()).to(eq([test_dealership]))
+    end
+  end
+
+
+
+
+
+
 end
